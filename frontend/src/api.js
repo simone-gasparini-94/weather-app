390d6d12f34b762337ef122b90bbd2e6beacd38a
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-02-17 15:57:33 +0100

    Host server on render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 export async function fetchWeather(location) {
     const url = 
-    `http://localhost:4000/${location}`;
+    `https://weather-app-y2ns.onrender.com/${location}`;
     const http = await fetch(url);
     if (!http.ok) throw new Error(`HTTP status ${http.status}`);
     const data = await http.json();
